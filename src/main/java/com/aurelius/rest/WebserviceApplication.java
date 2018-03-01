166bdebc8305381e33c3156db90249b781758587
Tan Kim Loong Ian <aurelius0523@gmail.com>
2018-03-02 01:25:00 +0800

    Added Spring Boot Admin for Monitoring. TODO - Add CPU metric

--- src/main/java/com/aurelius/rest/WebserviceApplication.java
+++ src/main/java/com/aurelius/rest/WebserviceApplication.java
@@ -3,7 +3,10 @@ package com.aurelius.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
 @SpringBootApplication
+@EnableAdminServer
 public class WebserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebserviceApplication.class, args);
